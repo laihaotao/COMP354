2b36a9451c27423f26042833eba53e61427ceb3e
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2017-05-13 20:25:55 -0400

    Added different card lists to the gui
    Includes bench, hand and active

--- src/main/java/ui/PlayerView.java
+++ src/main/java/ui/PlayerView.java
@@ -2,6 +2,9 @@ package ui;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 /**
  * Class used to display info about a player
@@ -9,13 +12,61 @@ import javafx.scene.layout.BorderPane;
  */
 public class PlayerView extends BorderPane {
 
+    //List that constains player hand cards
+    private HBox handCards;
+
+    //List that contains player bench cards
+    private HBox benchCards;
+
+    //Pane that constains the active card. This is not an HBox since their can only be one active pokemon card
+    private StackPane activeCard;
+
     //TODO this needs to take a player object
     public PlayerView(){
 
-        //For now, we just display label to show it works
-        //Should display actual info later on when player class is done
+        //VBox that constians the hand, bench and active pokemon
+        VBox centerCardArea = new VBox();
 
-        this.setCenter(new Label("Player"));
+        handCards = new HBox();
+        benchCards = new HBox();
+        activeCard = new StackPane();
+
+        //Add them in "reverse" order for them to display from bottom to top
+        centerCardArea.getChildren().addAll(activeCard, benchCards, handCards );
+
+        this.setCenter(centerCardArea);
+
+        refreshView();
+    }
+
+    public void refreshView(){
+
+        //TODO display actual player card info
+
+        //Add hand cards
+        handCards.getChildren().clear();
+
+        handCards.getChildren().add(new Label("Hand"));
+
+        //For now, just add 5 cardViews to make sure it displays
+        for(int i=0; i < 5; i++){
+            handCards.getChildren().add(new CardView());
+        }
+
+
+        //Add bench cards
+        benchCards.getChildren().clear();
+        benchCards.getChildren().add(new Label("Bench"));
+
+        //For now, just add 3 cardViews to make sure it displays
+        for(int i=0; i < 3; i++){
+            benchCards.getChildren().add(new CardView());
+        }
+
+
+        //set the current active card
+        activeCard.getChildren().clear();
+        activeCard.getChildren().add(new CardView());
     }
 
 }
