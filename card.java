3d04425be06582aef20c327591c989d4dff79a9e
Martin <martin.tseng1988@gmail.com>
2017-05-14 15:57:29 -0400

    Add files via upload

---
+++ card.java
@@ -0,0 +1,20 @@
+//this class is just to demo the Pikachu demo
+public class card {
+
+	private String name = "pikachu";
+	
+	public void messageFunction(){
+		System.out.println("Creating a New Class message");
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ pikachuDemo.java
@@ -0,0 +1,16 @@
+
+public class pikachuDemo extends card {
+	
+	
+	
+	
+	public String getName(){
+		
+		return super.getName();
+	}
+	
+	public void attack(){
+		
+		System.out.println("ATTACJ!");
+	}
+}
