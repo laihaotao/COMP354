2f4fb580501c999311eb70a39effdc027f788125
Frederic Desgreniers <fred@frde.me>
2017-05-15 19:06:45 -0400

    implements PlayerViewListener and passes events to GameBaord

--- src/main/java/ui/BoardView.java
+++ src/main/java/ui/BoardView.java
@@ -1,17 +1,24 @@
 package ui;
 
+import card.Card;
 import game.GameBoard;
+import game.Player;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Transform;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ui.events.PlayerViewListener;
 
 /**
  * Used to display info about the boardgame
  */
-public class BoardView extends BorderPane {
+public class BoardView extends BorderPane implements PlayerViewListener{
 
+    final static Logger logger = LogManager.getLogger(BoardView.class.getName());
+    
     private PlayerView[] playerViews;
 
     private GameBoard gameBoard;
@@ -30,6 +37,10 @@ public class BoardView extends BorderPane {
         //TODO Once we get a player class, we pass that to PlayerView
         playerViews[0] = new PlayerView(gameBoard.getPlayer1());
         playerViews[1] = new PlayerView(gameBoard.getPlayer2());
+
+        for (PlayerView playerView : playerViews) {
+            playerView.registerListener(this);
+        }
         
         //create end game button
         Button endTurnBtn = new Button("End Turn");
@@ -43,16 +54,18 @@ public class BoardView extends BorderPane {
         this.setTop(playerViews[1]);
         this.setBottom(playerViews[0]);
 
-        updateView();
+        refreshView();
     }
 
     /**
      * Updates the board view and player views to display new info
      */
-    public void updateView() {
+    public void refreshView() {
 
-        //TODO update player views
-
+        for (PlayerView playerView : playerViews) {
+            playerView.refreshView();
+        }
+        
         //We need to redo rotate every yupdate in case
         //player 2's pane changed dimensions
         PlayerView p2View = playerViews[1];
@@ -63,4 +76,21 @@ public class BoardView extends BorderPane {
 
     }
 
+    @Override
+    public void onHandCardClicked(Player player, Card card) {        
+        gameBoard.onHandCardClicked(player, card);
+        refreshView();
+    }
+
+    @Override
+    public void onBenchCardClicked(Player player, Card card) {
+        gameBoard.onBenchCardClicked(player, card);
+        refreshView();
+    }
+
+    @Override
+    public void onActiveCardClicked(Player player, Card card) {
+        gameBoard.onActiveCardClicked(player, card);
+        refreshView();
+    }
 }
