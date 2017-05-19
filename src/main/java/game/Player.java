6e701354a1a797e31e80beb934e34f3bfefbe1db
kawsara25 <kawsara25@yahoo.com>
2017-05-18 23:23:46 -0400

    Update Player.java

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,158 +1,213 @@
+package game;
 
-import java.util.*;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.Normalizer;
 
-import javax.sound.midi.SysexMessage;
+import card.Card;
+import card.pokemon.PikachuDemo;
+import card.pokemon.PokemonCard;
 
-import Card.CardType;
 /**
  * Created by frede on 2017-05-15.
- * modified by kawsara 2017-05-16
- *in progress
  */
 public class Player {
 	
-	private  ArrayList<Card> deck;// = new Card[60];
-	private  ArrayList<Card> prizes ;//= new Card[6];
-	private  ArrayList<Card> hand;
-	private   ArrayList<Card>bench ;//= new Card[5];;
-	private  ArrayList<Card> discardPile;
-	private ArrayList<Card> onTable;
-	private Card activePokemon;
-	private  ArrayList<Card>  pokemonCards;
-	private ArrayList<Card>  energyCards;
-	private  ArrayList<Card> trainerCards;
-	Scanner kb = new Scanner(System.in);
-	Random rand = new Random();
-
 	
-	public Player(ArrayList<Card> playerDeck)
-	{
-		//Each player gets 7 cards drawn randomly at the beginning of the game
+	
+	private Card [] playerDeck = new Card[60];
+	
+	
+	
+    //Will take the txt.file, read through it, and create the decks
+    public void createDeck() throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException{
+    	
+    	//IMPORTANT: The deck text files must be a folder called "decks". If you don't have one, please create one
+		//Card [] playerDeck = new Card [60];
 		
-		for(int i = 0 ; i<6; i++)
-		{
-			int  n = rand.nextInt(playerDeck.size());
-			prizes.add(playerDeck.remove(n));
-		}//end for loop
+		FileReader playerDeckFile = new FileReader("src/main/java/decks/deck1.ptcgo.txt");
+		FileReader opponentDeckFile = new FileReader("src/main/java/decks/deck1.ptcgo.txt");
 		
-		//Each player draws 7 cards at the beginning of the game and keeps their own hand hidden.
-		for(int i = 0 ; i<7; i++)
-		{
+		playerDeck = loadDeck(playerDeckFile);
+
+
+
+		
+		
+    	
+    }
+    
+    public Card[] loadDeck(FileReader file) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+    	
+    	PikachuDemo pika = new PikachuDemo();
+    	
+    	
+    	//Create Scanner to read deck.txt files
+		BufferedReader reader = new BufferedReader(file);
+		Card [] deck = new Card[60];
+		
+		//initiate variables to read the file
+		String line = "";
+		String [] lineParts = new String [10];
+		
+		//Read the first line first
+		line = reader.readLine();
+
+		//variables for the while loop
+		boolean isPokemonCard = false;
+		boolean isTrainerCard = false;
+		boolean isEnergyCard = false;
+		int deckIndex = 0;
+		
+		//While loop to read all the lines.
+		while (line != null){
 			
-			hand.add(playerDeck.remove(i));
-			if(hand.get(i).cardType == "Pokemon")
-				pokemonCards.add(hand.get(i));
+			//Game Rules: A player cannot have more than 60;
+			if (deckIndex>60){ 
+				System.out.println("You may not have more than 60 Cards");
+				break;
+			}
+			
+			
+			if (!line.isEmpty()){//if the line is not empty, we proceed
 				
-		}//end for loop
-		
-		chooseActivePokemon();
-	}
-	
-	/*
-	 * this is done only at the begining 
-	 */
-	public void chooseActivePokemon()
-	{
-		if (pokemonCards.size() >0)
-		{
-			System.out.println("these are the pokemon you have in hand");
+				lineParts = line.split(" ");
+				
+				//Checking which category
+				if (lineParts[0].equals("##Pokémon")){
+					isPokemonCard = true;
+					//isTrainerCard = false;
+					//isEnergyCard = false;
+				}
+				else if (lineParts[0].equals("##Trainer")){
+					//isPokemonCard = false;
+					isTrainerCard = true;
+					//isEnergyCard = false;
+					
+				}
+				else if (lineParts[0].equals("##Energy")){
+					//isPokemonCard = false;
+					//isTrainerCard = false;
+					isEnergyCard = true;
+				}
+				
+				else if (lineParts[0].equals("*")){
+					//Line contains pokemon Card information
+					
+					//Number of Cards of the same type
+					int numberOfCards = Integer.parseInt(lineParts[1]); 
+					if (!isEnergyCard){
+						if (numberOfCards>4){//Game rules: can't have more than 4 Cards of the same type
+							System.out.println("It is against the rules to have more than 4 Cards of the same type");
+							System.out.println(numberOfCards + line);
+						}
+					}
+					
+					//WILL CREATE 60 PIKACHUS FOR DEMO
+					String className = "PikachuDemo";  //must use fully qualified name
 
-			for(int i = 0 ; i<pokemonCards.size(); i++)
-			{
-				System.out.println(i+1 +": "+pokemonCards.get(i).name);
-			}//end for loop
-			System.out.println("chose which card you want to use ");
-			int pokNum ;
-			do
-			{
-				System.out.println("chose which card you want to use as your active pokemon ");
-				System.out.println("Enter the number");
-				pokNum  = kb.nextInt();
-			}while(pokNum <1 || pokNum > pokemonCards.size());
-			activePokemon = pokemonCards.get(pokNum-1);
-			System.out.println(" your active pokemon is: " + activePokemon.name);
+					/* NOTE:PLEASE DO NOT DELETE THIS OR MODIFY THIS YET. THIS ADDS THE CORRESPONDING POKEMON BASED ON THE STRING THAT IT READS
+					 * EXAMPLE: If it reads "Glameow BKP 93" It will create a class object of GlameowbBKP. If you uncomment this
+					 * You will get an error as we have not created the pokemons yet.
+					 * 
+					 * Martin Tseng
+					//Clean any accents or apostrophe in the name
+					String className = convertLineToClassName(line);;
+					if (isEnergyCard){
+						
+						if(className.contains("Grass")){
+							className = "Grass";
+						}
+						else if(className.contains("Fire")){
+							className = "Fire";
+						}
+						else if(className.contains("Water")){
+							className = "Water";
+						}
+						else if(className.contains("Lightning")){
+							className = "Lightning";
+						}
+						else if(className.contains("Psychic")){
+							className = "Psychic";
+						}
+						else if(className.contains("Fighting")){
+							className = "Fighting";
+						}
+						else if(className.contains("Darkness")){
+							className = "Darkness";
+						}
+						else if(className.contains("Metal")){
+							className = "Metal";
+						}
+						else if(className.contains("Colorless")){
+							className = "Colorless";
+						}
+						else if(className.contains("Fairy")){
+							className = "Fairy";
+						}
+						else if(className.contains("Dragon")){
+							className = "Dragon";
+						}
+						
+					}
+					*/
+					
+					//add Cards
+					
+					for (int i=0; i < numberOfCards;i++){//Create the number of Cards indicated in the line read
+						
+						
+						Class<?> cls = Class.forName("card.pokemon."+className);//must use fully qualified name
+						Object clsInstance = cls.newInstance();
+						deck[deckIndex] = (Card) clsInstance;
+						deckIndex +=1;
+						
+
+					}// end of for loop
+				}
+			}
+
+			line = reader.readLine(); //move to the next line
+		} // end of the While loop
+		
+		if (isPokemonCard == false){
+			System.out.println("you deck needs at least a pokemon Card");
+			
 		}
-		else 
-			System.out.println(" there are no pokemon in your hand");
-			
+		return deck;
+		
+		
+		
 	}
-
 	
-	public void putCardOnBench()
-	{
-		if (hand.size() >0)
-		{
-			System.out.println("these are the Cards you have in hand");
-
-			for(int i = 0 ; i<hand.size(); i++)
-			{
-				System.out.println(i+1 +": "+hand.get(i).name);
-			}//end for loop
-			//System.out.println("chose which card you want to add t your bench");
-			int pokNum ;
-			do
-			{
-				System.out.println("chose which card you want to add t your bench ");
-				System.out.println("Enter the number");
-				pokNum  = kb.nextInt();
-			}while(pokNum <1 || pokNum > hand.size());
-			bench.add(hand.remove(pokNum-1));
-			
+	
+	
+	//This method is used to clean the line to make it easier to read the string and to convert it to a className
+	public String convertLineToClassName (String string){
+		string = string.replaceAll("\\*", "");
+		string = Normalizer.normalize(string, Normalizer.Form.NFD);
+		string = string.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+		string = string.replaceAll("'", "");
+		string = string.replaceAll(" ", "");
+		string = string.replaceAll("-", "");
+		string = string.replaceAll("\\d", "");
+		return string;
+		
+	}
+	
+	
+	public void printDeck(){
+		//Good for testing
+		
+		//Print out the whole deck for testing
+		for (int i=0; i<playerDeck.length;i++){
+			System.out.println("Deck - Index: " + i + " " + playerDeck[i]);
 		}
-		else 
-			System.out.println(" there are no card in your hand");
-			
+		
 		
 	}
-	
-	
-	public void pickFromPrizeCard()
-	{
-		// pick a card and then add it to hand
-		if (prizes.size() >0)
-		{
-			System.out.println("these are the Cards you have as prizes");
-
-			
-			//System.out.println("chose which card you want to add t your bench");
-			int pokNum ;
-			
-			do
-			{
-				System.out.println("chose which card you want to add t your bench ");
-				System.out.println("Enter a number between 1 and " + prizes.size());
-				pokNum  = kb.nextInt();
-			}while(pokNum <1 || pokNum > prizes.size());
-			hand.add(prizes.remove(pokNum-1));
-			
-		}
-		else 
-			System.out.println(" you have no cards as prizes");
-			
-		
-	}
-	public void drawTopSixCard()
-	{
-		
-	}
-	/*
-	 * done when we want to change our active pokemon to another
-	 */
-	public void swapAcTivePokemon()
-	{
-		
-	}
-	
-	public void evolveAcTivePokemon()
-	{
-		
-	}
-	public void attackOpponent(Card opponent)
-	{
-		
-	}
-	
-	
+    
 
 }
+
