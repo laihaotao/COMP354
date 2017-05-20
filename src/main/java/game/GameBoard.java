7b39ad5ebf3373e06f7670cd10eb15f366e3bc3b
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2017-05-20 18:56:44 -0400

    Added some game logic such as turns and adding pokemons to bench and active spot

--- src/main/java/game/GameBoard.java
+++ src/main/java/game/GameBoard.java
@@ -1,9 +1,9 @@
 package game;
 
 import card.Card;
+import card.pokemon.PokemonCard;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import ui.BoardView;
 
 /**
  * Created by frede on 2017-05-15.
@@ -13,32 +13,83 @@ public class GameBoard {
   final static Logger logger = LogManager.getLogger(GameBoard.class.getName());
   
   private Player[] players;
-  
+
+  private int currentTurn = 0;
+
+  private Card selectedCard = null;
+  private CardLocation selectedCardLocation = null;
+
   public GameBoard(Player p1, Player p2) {
       players = new Player[2];
       players[0] = p1;
       players[1] = p2;
   }
-  
+
+  public void setSelectedCard(Card card, CardLocation location){
+    if(selectedCard != null) {
+      selectedCard.setSelected(false);
+    }
+
+    selectedCard = card;
+    if(selectedCard != null){
+      selectedCard.setSelected(true);
+    }
+    selectedCardLocation = location;
+  }
+
   public void onHandCardClicked(Player player, Card card){
     int playerNum = (player == players[0])?1:2;
     logger.debug("Player"+playerNum+" has clicked a card in it's hand");
-    
-      //TODO add game logic here for when a hand card is clicked
+
+    if(card != null) {
+      setSelectedCard(card, CardLocation.HAND);
+    }
+
   }
 
   public void onBenchCardClicked(Player player, Card card){
     int playerNum = (player == players[0])?1:2;
     logger.debug("Player"+playerNum+" has clicked a card in it's bench");
 
-    //TODO add game logic here for when a hand card is clicked
+    //Player is trying to place pokemon card on bench
+    if(selectedCard != null && selectedCardLocation == CardLocation.HAND && selectedCard instanceof PokemonCard){
+      if(player.getHand().remove(selectedCard)){
+        player.getBench().add(selectedCard);
+        setSelectedCard(null, null);
+      }
+
+      return;
+    }
+
   }
 
   public void onActiveCardClicked(Player player, Card card){
     int playerNum = (player == players[0])?1:2;
     logger.debug("Player"+playerNum+" has clicked the active pokemon");
 
-    //TODO add game logic here for when a hand card is clicked
+    //player is trying to place pokemon on active slot
+    if(player.getActivePokemon() == null && selectedCard != null && selectedCardLocation== CardLocation.HAND && selectedCard instanceof PokemonCard){
+
+      if(player.getHand().remove(selectedCard)) {
+        player.setActivePokemon(selectedCard);
+        setSelectedCard(null, null);
+      }
+
+      return;
+    }
+  }
+
+  public void onEndTurnButtonClicked(){
+    nextTurn();
+  }
+
+  public void nextTurn(){
+    Player currentPlayer = getCurrentTurnPlayer();
+    currentPlayer.putCardInHand();
+
+    currentTurn = (currentTurn + 1)%2;
+
+    //TODO process AI turn
   }
   
   public Player[] getPlayers(){
@@ -51,5 +102,13 @@ public class GameBoard {
   
   public Player getPlayer2(){
     return players[1];
+  }
+
+  public Player getCurrentTurnPlayer(){
+    return players[currentTurn];
+  }
+
+  public Player getOppositeTurnPlayer(){
+    return players[(currentTurn+1)%2];
   }
 }
