5fc7d14911b5fadb1b9d7ed15cd56348769b3365
Frederic Desgreniers <fred@frde.me>
2017-05-23 16:36:17 -0400

    Added comments

--- src/main/java/parser/abilities/AbilitiesParser.java
+++ src/main/java/parser/abilities/AbilitiesParser.java
@@ -23,22 +23,35 @@ import parser.ui.TokenPane;
 
 /**
  * Created by frede on 2017-05-23.
+ * 
+ * Parses abilities from text file using a LanguageTokenizer
  */
 public class AbilitiesParser {
 
   static Logger log = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
   
   LanguageTokenizer tokenizer;
-  
+
+  /**
+   * 
+   * @param fileName File name that needs to be parsed
+   */
   public AbilitiesParser(String fileName){
     tokenizer = new LanguageTokenizer(fileName);
   }
-  
+
+  /**
+   * Parse a file into a series of abilities
+   * @return List of all abilities parsed
+   */
   public List<AbilityTemplate> parse() {
+    //Get a list of all tokens in the file using the tokenizer
     List<TokenScope> scopes = tokenizer.tokenize();
     
+    //Where the abilities are going to be stored
     List<AbilityTemplate> templates = new ArrayList<>();
     
+    //For each "Scope" token, process it as a null / parent scope
     scopes.forEach((scope)->{
         templates.add(parseNullScope(scope));
     });
@@ -67,21 +80,30 @@ public class AbilitiesParser {
     
     return templates;
   }
-  
+
+  /**
+   * Parse a parent scope
+   * @param scope scope to parse
+   * @return
+   */
   private AbilityTemplate parseNullScope(TokenScope scope){
 
+    //get a token stream for this scope
     TokenStream tokenStream = new TokenStream(scope.tokens);
     
+    //First token should always be the name of the ability
     String name = tokenStream.validateTokenString().value;
     if(name == null){
       log.error("Name token needs to be a string: " + tokenStream.getNextToken());
       return null;
     }
     
+    //blank ability template
     AbilityTemplate template = new AbilityTemplate(name);
     
     //log.debug(" " + name+ " ");
     
+    //Go through tokens, parsing them into ability parts
     Token token = null;
     while((token = tokenStream.getNextToken()) != null) {
         if(token instanceof TokenString){
@@ -105,7 +127,12 @@ public class AbilitiesParser {
     
     return template;
   }
-  
+
+  /**
+   * Go through tokens until we reach the desired one
+   * @param stream
+   * @param type
+   */
   private void waitUntil(TokenStream stream, TokenType type){
     Token token = null;  
     while((token = stream.getNextToken()) != null){
@@ -114,7 +141,12 @@ public class AbilitiesParser {
         }
     }
   }
-  
+
+  /**
+   * Parse the deck ability part
+   * @param tokenStream
+   * @return
+   */
   private AbilityPart parsePartDeck(TokenStream tokenStream){
     
       //log.debug("---Deck---");
@@ -138,7 +170,12 @@ public class AbilitiesParser {
       //log.debug("Choice: "+Formatting.toSafeString(choice));
       return new AbilityPartDeck(targetPart, destinationPart, choice, amount);
   }
-  
+
+  /**
+   * Parse the Dam (damage) ability part
+   * @param tokenStream
+   * @return
+   */
   private AbilityPart parseDamPart(TokenStream tokenStream){
     //log.debug("---Dam---");
 
@@ -151,7 +188,12 @@ public class AbilitiesParser {
     
     return new AbilityPartDam(targetPart, amount);
   }
-  
+
+  /**
+   * Parse the Draw ability part
+   * @param tokenStream
+   * @return
+   */
   private AbilityPart parseDrawPart(TokenStream tokenStream){
     TargetProperty target = new TargetProperty(tokenStream.validateTokenString(), null);
     
