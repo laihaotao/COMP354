90ecfe8957da12d8754489a061bf2cabe90c2430
Frederic Desgreniers <fred@frde.me>
2017-05-23 14:49:29 -0400

    Added Draw ability part

--- src/main/java/parser/abilities/AbilitiesParser.java
+++ src/main/java/parser/abilities/AbilitiesParser.java
@@ -13,6 +13,7 @@ import parser.commons.DestinationProperty;
 import parser.commons.TargetProperty;
 import parser.tokenizer.LanguageTokenizer;
 import parser.tokenizer.Token;
+import parser.tokenizer.TokenInteger;
 import parser.tokenizer.TokenScope;
 import parser.tokenizer.TokenStream;
 import parser.tokenizer.TokenString;
@@ -50,16 +51,16 @@ public class AbilitiesParser {
     TreeItem tokenItemPane = new TokenPane(scopes.toArray(new Token[0]));
     
     TreeView tokenView = new TreeView<>(tokenItemPane);
-    tokenView.setMinWidth(400);
+    tokenView.setMinWidth(600);
     mainView.setLeft(tokenView);
     
     TreeItem abilityItemPane = new AbilityTemplatePane((templates.toArray(new AbilityTemplate[0])));
     TreeView abilityView = new TreeView(abilityItemPane);
-    abilityView.setMinWidth(400);
+    abilityView.setMinWidth(600);
     mainView.setRight(abilityView);
     
     
-    stage.setScene(new Scene(mainView, 800,800));
+    stage.setScene(new Scene(mainView, 1200,800));
     stage.show();
     
     
@@ -91,6 +92,9 @@ public class AbilitiesParser {
               break;
             case  "dam":
               template.parts.add(parseDamPart(tokenStream));
+              break;
+            case "draw":
+              template.parts.add(parseDrawPart(tokenStream));
               break;
             default:
               waitUntil(tokenStream, TokenType.SEPERATOR);
@@ -125,7 +129,10 @@ public class AbilitiesParser {
       }
       
       Token amount = tokenStream.getNextToken();
-
+      if(!(amount instanceof TokenScope) && !(amount instanceof TokenInteger))
+      {
+        tokenStream.backtrack();
+      }
       //log.debug(targetPart);
       //log.debug(destinationPart);
       //log.debug("Choice: "+Formatting.toSafeString(choice));
@@ -145,6 +152,17 @@ public class AbilitiesParser {
     return new AbilityPartDam(targetPart, amount);
   }
   
+  private AbilityPart parseDrawPart(TokenStream tokenStream){
+    TargetProperty target = new TargetProperty(tokenStream.validateTokenString(), null);
+    
+    Token amount = tokenStream.getNextToken();
+    if(!(amount instanceof TokenScope) && !(amount instanceof TokenInteger))
+    {
+      tokenStream.backtrack();
+    }
+    
+    return new AbilityPartDraw(target, amount);
+  }
 
   
 }
---
+++ src/main/java/parser/abilities/AbilityPartDraw.java
@@ -0,0 +1,23 @@
+package parser.abilities;
+
+import parser.commons.TargetProperty;
+import parser.commons.TokenProperty;
+import parser.tokenizer.Token;
+
+/**
+ * Created by frede on 2017-05-23.
+ */
+public class AbilityPartDraw extends AbilityPart{
+
+  private TargetProperty target;
+  private Token amount;
+  
+  public AbilityPartDraw(TargetProperty target, Token amount) {
+    super("Draw");
+    this.target = target;
+    this.amount = amount;
+    
+    properties.add(target);
+    properties.add(new TokenProperty("Amount", amount));
+  }
+}
