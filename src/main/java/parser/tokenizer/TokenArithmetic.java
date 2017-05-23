81f9ac13ef0277920e103d9ca742ab968cb56ea0
Frederic Desgreniers <fred@frde.me>
2017-05-23 09:10:19 -0400

    Supports left and right values

--- src/main/java/parser/tokenizer/TokenArithmetic.java
+++ src/main/java/parser/tokenizer/TokenArithmetic.java
@@ -7,13 +7,17 @@ public class TokenArithmetic extends Token{
   
   public final ArithmeticType type;
   
-  public TokenArithmetic(int endLocation, ArithmeticType type) {
+  private Token leftValue, rightValue;
+  
+  public TokenArithmetic(int endLocation, ArithmeticType type, Token leftValue, Token rightValue) {
     super(TokenType.ARITHMETIC, endLocation);
     this.type = type;
+    this.leftValue = leftValue;
+    this.rightValue = rightValue;
   }
   
   public String toString(){
-    return super.toString() + "Arithmetic -> "+ArithmeticType.MULTIPLICATION;
+    return super.toString() + "Arithmetic -> "+ leftValue + " " + type + " " + rightValue;
   }
   
   
