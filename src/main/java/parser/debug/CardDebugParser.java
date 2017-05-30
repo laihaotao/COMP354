4f50993df55c34bf0a3e015f57ea195451d6ee90
Frederic Desgreniers <fred@frde.me>
2017-05-30 17:56:26 -0400

    Better formatting

--- src/main/java/parser/debug/CardDebugParser.java
+++ src/main/java/parser/debug/CardDebugParser.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import parser.abilities.AbilitiesParser;
+import parser.abilities.AbilityPart;
+import parser.abilities.AbilityTemplate;
 import parser.cards.CardParser;
 import parser.tokenizer.LanguageTokenizer;
 import parser.tokenizer.TokenScope;
@@ -15,14 +18,20 @@ import parser.tokenizer.TokenStream;
 public class CardDebugParser {
   
   LanguageTokenizer tokenizer;
+  AbilityTemplate[] abilityTemplates;
 
   static Logger log = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
   
   public CardDebugParser(String fileName){
       tokenizer = new LanguageTokenizer(fileName);
+      AbilitiesParser abilitiesParser = new AbilitiesParser("abilities.txt");
+      abilityTemplates = abilitiesParser.parse();
   }
   
   public void parse(){
+
+    
+    
       List<TokenScope> lineScopes = tokenizer.tokenize();
       lineScopes.forEach((lineScope)->{
           parseCard(lineScope);
@@ -65,7 +74,7 @@ public class CardDebugParser {
   }
   
   private void parseBasicPokemon(String name, TokenStream tokenStream){
-      log.debug(name+": ");
+      log.debug(name+" {");
     
       if(tokenStream.validateTokenString("cat") == null){
           log.error(name + " No cat");
@@ -86,13 +95,13 @@ public class CardDebugParser {
           String retreatCostName = tokenStream.validateTokenString().value;
         int retreatCostNum = tokenStream.validateTokenInteger().value;
         retreatCost.addEnergy(retreatCostName,retreatCostNum);
-          log.debug("retreat: " + retreatCost);
+          log.debug("\tretreat: " + retreatCost);
 
-        
       }
       
       
       if(tokenStream.validateTokenString("attacks") != null){
+        log.debug("\tAttacks {");
         tokenStream.backtrack();
         while(tokenStream.getNextToken() != null) {
           EnergyCost cost = new EnergyCost();
@@ -107,11 +116,13 @@ public class CardDebugParser {
           } while (tokenStream.validateTokenSeparator() != null);
 
           int abilityLineNum = tokenStream.validateTokenInteger().value;
-          log.debug(abilityLineNum + " : " + cost);
+          log.debug("\t\t"+abilityTemplates[abilityLineNum].name + " : " + cost);
           
           
         }
+        log.debug("\t}");
       }
+      log.debug("}");
   }
 
 }
