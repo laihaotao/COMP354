c4dc81b3a0c96666b3e195106c79dee5b5a39676
kawsara25 <kawsara25@yahoo.com>
2017-07-14 00:29:39 -0400

    pulling into branch sara

--- src/main/java/game/GameBoard.java
+++ src/main/java/game/GameBoard.java
@@ -22,6 +22,7 @@ import java.util.*;
 
 public class GameBoard {
 
+
     enum CardLocation {
         DECK, HAND, BENCH, ACTIVE, DISCARD,
     }
---
+++ src/test/java/Player/TestButtonClicked.java
@@ -0,0 +1,66 @@
+package Player;
+
+import card.Card;
+import entry.Config;
+import game.GameBoard;
+import game.Player;
+import game.ai.IntelligentPlayer;
+import org.junit.Test;
+import parser.cards.CardParser;
+import parser.cards.DeckParser;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by kawsa on 7/13/2017.
+ */
+
+public class TestButtonClicked {
+    String deck1FileNm = "deck1.txt";
+    String deck2FileNm = "deck2.txt";
+
+    CardParser cardParser;
+    DeckParser deck1Parser ;
+    DeckParser deck2Parser;
+    List<Card> player1Deck ;
+    List<Card> player2Deck;
+    Player player1 ;//= new Player(player1Deck);
+    Player player2;
+    @Test
+    public void testRetreatButtonClicked() throws ClassNotFoundException,IOException {
+
+        String deck1FileNm = "deck1.txt";
+        String deck2FileNm = "deck2.txt";
+
+        cardParser = new CardParser(Config.FILE_PATH_CARDS_TXT);
+        deck1Parser = new DeckParser(deck1FileNm, cardParser);
+        deck2Parser = new DeckParser(deck2FileNm, cardParser);
+
+        player1Deck = deck1Parser.getDeck();
+        player2Deck = deck2Parser.getDeck();
+
+        Player player1 = new Player(player1Deck);
+        Player player2 = new IntelligentPlayer(player2Deck);
+
+        player1.setName("human player");
+        player2.setName("AI player");
+
+
+        GameBoard gm = new GameBoard(player1,player2);
+        player1.chooseActivePokemon();
+        player2.checkForPokemon();
+        gm.onRetreatButtonClicked(player1);
+        gm.onRetreatButtonClicked(player2);
+
+        assertEquals(null, player1.getActivePokemon() );
+
+        assertEquals(null, player2.getActivePokemon());
+
+    }
+
+
+}
