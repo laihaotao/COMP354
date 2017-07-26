27973c0c725a9efdd76524f7c50637cc888f1757
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2017-07-25 20:32:34 -0400

    implemented evolvefrom filter

--- src/main/java/parser/abilities/filters/FilterEvolveFrom.java
+++ src/main/java/parser/abilities/filters/FilterEvolveFrom.java
@@ -1,5 +1,11 @@
 package parser.abilities.filters;
 
+import card.Card;
+import card.PokemonCard;
+import game.GameBoard;
+import game.Player;
+import java.util.ArrayList;
+import java.util.List;
 import parser.abilities.properties.TargetProperty;
 
 public class FilterEvolveFrom extends Filter {
@@ -9,6 +15,24 @@ public class FilterEvolveFrom extends Filter {
     public FilterEvolveFrom(TargetProperty target){
 
         this.target = target;
+    }
+
+    public List<Card> evaluate(GameBoard targetBoard, Player owner, List<Card> cards){
+        List<Card> filteredCards = new ArrayList<>();
+        Card targetCard = owner.getTarget(targetBoard, target);
+        if(targetCard instanceof PokemonCard) {
+            PokemonCard targetPokemon = (PokemonCard)targetCard; 
+            cards.forEach(card -> {
+                if (card instanceof PokemonCard) {
+                    PokemonCard pokemonCard = (PokemonCard)card;
+                    if(pokemonCard.getEvolvesFrom().equalsIgnoreCase(targetPokemon.getCardName())){
+                        filteredCards.add(card);    
+                    }
+                    
+                }
+            });
+        }
+        return filteredCards;
     }
     
     public String toString(){
