d4472ebf62d13351096f01d9a5e8e05de34d1f58
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2017-08-03 23:53:32 -0400

    Added category filter

--- src/main/java/parser/abilities/filters/FilterCategory.java
+++ src/main/java/parser/abilities/filters/FilterCategory.java
@@ -1,5 +1,10 @@
 package parser.abilities.filters;
 
+import card.Card;
+import card.PokemonCard;
+import game.GameBoard;
+import game.Player;
+
 public class FilterCategory extends Filter{
     private String category;
     
@@ -7,7 +12,13 @@ public class FilterCategory extends Filter{
 
         this.category = category;
     }
-
+    public boolean evaluate(GameBoard targetBoard, Player owner, Card card){
+        if(card instanceof PokemonCard){
+            PokemonCard pokemonCard = (PokemonCard)card;
+            return pokemonCard.getPokemonType().equalsIgnoreCase(category);
+        }
+        return false;
+    }
     public String toString(){
         return "Filters cards of type "+category;
     }
